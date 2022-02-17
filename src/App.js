8f1a40e126ec5814426bcc0c83ac36690ad84f1b
shika7200 <shive1997me@gmail.com>
2022-02-18 00:17:27 +0700

    добавил пагинацию

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Routes, Route} from 'react-router-dom'; 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+
+  <div> 
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-     <HomePage/>
+<Routes>
+    < Route exact path="/hats" element={<HatsPage/>} />
+     < Route  exact path="/" element={<HomePage/>}/> 
+
+     </Routes> 
     </div>
   );
 }
