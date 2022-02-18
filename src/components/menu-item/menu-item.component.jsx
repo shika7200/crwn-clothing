3d6c5b8de411d3940a946deb4b83744f0f4a3d66
shika7200 <shive1997me@gmail.com>
2022-02-18 13:54:27 +0500

    сделал не рабочий переход по ссылке

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -8,10 +8,10 @@ const withRouter = WrappedComponent => props => {
    };
 
 
-const MenuItem = ({title, imageUrl, size }) => (
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match }) => (
 
     <div
-    className={`${size} menu-item`}>
+    className={`${size} menu-item`} onClick={()=> history.push(`${match.url}${linkUrl}`)}>
         <div  className='background-image'
         style={{
             backgroundImage: `url(${imageUrl})`
