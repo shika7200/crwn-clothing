c4ee9c7e0ec3ce39b42d2dc6d6e9f863bcdc1218
uguru <shive1997me@gmail.com>
2022-02-19 23:53:31 +0700

    сделал страничку магазина

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@ import React from 'react';
 import { Routes, Route} from 'react-router-dom'; 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
 
-  <div> 
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
 <Routes>
-    < Route exact path="/hats" element={<HatsPage/>} />
-     < Route  exact path="/" element={<HomePage/>}/> 
+< Route  exact path="/" element={<HomePage/>}/> 
+    < Route exact path="/shop" element={<ShopPage/>} />
+ 
 
      </Routes> 
     </div>
